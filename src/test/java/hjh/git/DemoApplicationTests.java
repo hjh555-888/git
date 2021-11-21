07784e161d380bc2c51ca9f80581d63230c8e6f9
Joker <885287022@qq.com>
2021-11-21 10:21:57 +0800

    hot-fix commit

---
+++ src/test/java/hjh/git/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hjh.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
