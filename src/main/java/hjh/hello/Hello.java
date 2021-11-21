e7789ed0c4a28570008319287c7c818ea80d21e3
Joker <885287022@qq.com>
2021-11-21 10:26:06 +0800

    master commit2

--- src/main/java/hjh/hello/Hello.java
+++ src/main/java/hjh/hello/Hello.java
@@ -11,5 +11,7 @@ public class Hello {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello master-test");
+
     }
 }
