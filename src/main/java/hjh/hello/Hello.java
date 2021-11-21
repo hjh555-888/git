6d8ec4804e9b1f5a1e93bfb7f8c02b82b36d9596
Joker <885287022@qq.com>
2021-11-21 10:07:05 +0800

    second commit

---
+++ src/main/java/hjh/hello/Hello.java
@@ -0,0 +1,13 @@
+package hjh.hello;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author shkstart
+ * @create 2021-11-2110:09
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
